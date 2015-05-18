be640253744e711cbb762303032a366b1a2f2bda
David Rojas <drojascamaggi@gmail.com>
2015-05-17 21:57:44 -0300

    version bump

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject doctest "0.1.0"
+(defproject doctest "0.1.1"
   :description "Python-like doctest functionality for Clojure."
   :url "https://github.com/drojas/doctest"
   :license {:name "MIT License"
             :url "http://www.opensource.org/licenses/mit-license.php"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :scm {:name "git"
+        :url "https://github.com/drojas/doctest"})
